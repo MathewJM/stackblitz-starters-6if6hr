9ec83a33385f81770ea9c188c95dfedfce916d24
MathewJM <41521783+MathewJM@users.noreply.github.com>
2023-07-20 01:21:51 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div class="container">
+      <h1>Hello StackBlitz! &trade;</h1>
+      <p>Start editing to see some magic happen :)</p>
+      <p>Mathew JM &copy;</p>
+    </div>
+  );
+}
